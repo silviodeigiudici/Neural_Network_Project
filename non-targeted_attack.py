d55e41febf67fded130d05a1980d17290c0f462f
silviodeigiudici <silviodeigiudici@gmail.com>
2019-03-28 20:02:47 +0100

    fix again

--- non-targeted_attack.py
+++ non-targeted_attack.py
@@ -51,25 +51,24 @@ def get_max_class_new(preds, dict):
 def print_images(images, file):
     f = open(file, "r")
     s = f.read()
-    if not s:
-      print("\nNo image to show\n")
-    else:
-      list = s.strip().split(",")
-      img_index = int(list[0].strip())
-      number_of_pixel = int((len(list) - 1)/5)
-      index = 1
-      image = images[img_index]
-      plt.imshow(image)
-      plt.show()
-      for i in range(0, number_of_pixel):
-          row = int(list[index])
-          col = int(list[index + 1])
-          rgb = int(list[index + 2]), int(list[index + 3]), int(list[index + 4])
-          image[row][col] = rgb
-          index += 5
-      plt.imshow(image)
-      plt.show()
     f.close()
+    images_string = s.strip().split("\n")
+    for image in images_string:
+        list = image.strip().split(",")
+        img_index = int(list[0].strip())
+        number_of_pixel = int((len(list) - 1)/5)
+        index = 1
+        image = images[img_index]
+        plt.imshow(image)
+        plt.show()
+        for i in range(0, number_of_pixel):
+            row = int(list[index])
+            col = int(list[index + 1])
+            rgb = int(list[index + 2]), int(list[index + 3]), int(list[index + 4])
+            image[row][col] = rgb
+            index += 5
+        plt.imshow(image)
+        plt.show()
 
 #######################################
 #FUNCTIONS DIFFERENTIAL EVOLUTION
@@ -108,12 +107,12 @@ def get_random_input():
 
     return x1, y1, r1, g1, b1, x2, y2, r2, g2, b2, x3, y3, r3, g3, b3, x4, y4, r4, g4, b4, x5, y5, r5, g5, b5
 
-def new_par(pop, f, limit, i_parameter, population, best):
-    a = pop[randint(0, population - 1)][i_parameter]
-    b = pop[randint(0, population - 1)][i_parameter]
-    return (best[i_parameter] + f*(a - b)) % limit
+def new_par(population, F, limit, i_parameter, num_population, best):
+    a = population[randint(0, num_population - 1)][i_parameter]
+    b = population[randint(0, num_population - 1)][i_parameter]
+    return (best[i_parameter] + F*(a - b)) % limit
 
-def set_image(pop_array, p, input):
+def set_image(matrix, p, input):
     row1 = int(input[0])
     col1 = int(input[1])
     r1 = int(input[2])
@@ -145,92 +144,92 @@ def set_image(pop_array, p, input):
     b5 = int(input[24])
 
 
-    store1 = copy.deepcopy(pop_array[p][row1][col1])
-    store2 = copy.deepcopy(pop_array[p][row2][col2])
-    store3 = copy.deepcopy(pop_array[p][row3][col3])
-    store4 = copy.deepcopy(pop_array[p][row4][col4])
-    store5 = copy.deepcopy(pop_array[p][row5][col5])
+    store1 = copy.deepcopy(matrix[p][row1][col1])
+    store2 = copy.deepcopy(matrix[p][row2][col2])
+    store3 = copy.deepcopy(matrix[p][row3][col3])
+    store4 = copy.deepcopy(matrix[p][row4][col4])
+    store5 = copy.deepcopy(matrix[p][row5][col5])
 
 
-    pop_array[p][row1][col1] = r1, g1, b1
-    pop_array[p][row2][col2] = r2, g2, b2
-    pop_array[p][row3][col3] = r3, g3, b3
-    pop_array[p][row4][col4] = r4, g4, b4
-    pop_array[p][row5][col5] = r5, g5, b5
+    matrix[p][row1][col1] = r1, g1, b1
+    matrix[p][row2][col2] = r2, g2, b2
+    matrix[p][row3][col3] = r3, g3, b3
+    matrix[p][row4][col4] = r4, g4, b4
+    matrix[p][row5][col5] = r5, g5, b5
 
     return store1, store2, store3, store4, store5
 
 
-def unset_images(pop_store, pop_array, population, new_pop):
-    for i in range(0, population):
-        store1, store2, store3, store4, store5 = pop_store[i]
+def unset_images(old_pixel_store, matrix, num_population, new_population):
+    for i in range(0, num_population):
+        store1, store2, store3, store4, store5 = old_pixel_store[i]
 
-        row1 = int(new_pop[i][0])
-        col1 = int(new_pop[i][1])
+        row1 = int(new_population[i][0])
+        col1 = int(new_population[i][1])
 
-        row2 = int(new_pop[i][5])
-        col2 = int(new_pop[i][6])
+        row2 = int(new_population[i][5])
+        col2 = int(new_population[i][6])
 
-        row3 = int(new_pop[i][10])
-        col3 = int(new_pop[i][11])
+        row3 = int(new_population[i][10])
+        col3 = int(new_population[i][11])
 
-        row4 = int(new_pop[i][15])
-        col4 = int(new_pop[i][16])
+        row4 = int(new_population[i][15])
+        col4 = int(new_population[i][16])
 
-        row5 = int(new_pop[i][20])
-        col5 = int(new_pop[i][21])
+        row5 = int(new_population[i][20])
+        col5 = int(new_population[i][21])
 
-        pop_array[i][row1][col1] = store1
-        pop_array[i][row2][col2] = store2
-        pop_array[i][row3][col3] = store3
-        pop_array[i][row4][col4] = store4
-        pop_array[i][row5][col5] = store5
+        matrix[i][row1][col1] = store1
+        matrix[i][row2][col2] = store2
+        matrix[i][row3][col3] = store3
+        matrix[i][row4][col4] = store4
+        matrix[i][row5][col5] = store5
 
-def new_matrix(img, population):
-    pop_array = np.ndarray((population, 32, 32, 3))
-    population_indeces = range(0, population)
+def new_matrix(img, num_population):
+    matrix = np.ndarray((num_population, 32, 32, 3))
+    population_indeces = range(0, num_population)
     for p in population_indeces:
-        pop_array[p] = copy.deepcopy(img[0])
-    return pop_array
+        matrix[p] = copy.deepcopy(img[0])
+    return matrix
 
-def new_son(pop, f, range_pixel, range_rgb, population, best):
+def new_son(population, F, range_pixel, range_rgb, num_population, best):
 
-    row1 = new_par(pop, f, range_pixel, 0, population, best)
-    col1 = new_par(pop, f, range_pixel, 1, population, best)
-    r1 = new_par(pop, f, range_rgb, 2, population, best)
-    g1 = new_par(pop, f, range_rgb, 3, population, best)
-    b1 = new_par(pop, f, range_rgb, 4, population, best)
+    row1 = new_par(population, F, range_pixel, 0, num_population, best)
+    col1 = new_par(population, F, range_pixel, 1, num_population, best)
+    r1 = new_par(population, F, range_rgb, 2, num_population, best)
+    g1 = new_par(population, F, range_rgb, 3, num_population, best)
+    b1 = new_par(population, F, range_rgb, 4, num_population, best)
 
-    row2 = new_par(pop, f, range_pixel, 5, population, best)
-    col2 = new_par(pop, f, range_pixel, 6, population, best)
-    r2 = new_par(pop, f, range_rgb, 7, population, best)
-    g2 = new_par(pop, f, range_rgb, 8, population, best)
-    b2 = new_par(pop, f, range_rgb, 9, population, best)
+    row2 = new_par(population, F, range_pixel, 5, num_population, best)
+    col2 = new_par(population, F, range_pixel, 6, num_population, best)
+    r2 = new_par(population, F, range_rgb, 7, num_population, best)
+    g2 = new_par(population, F, range_rgb, 8, num_population, best)
+    b2 = new_par(population, F, range_rgb, 9, num_population, best)
 
-    row3 = new_par(pop, f, range_pixel, 10, population, best)
-    col3 = new_par(pop, f, range_pixel, 11, population, best)
-    r3 = new_par(pop, f, range_rgb, 12, population, best)
-    g3 = new_par(pop, f, range_rgb, 13, population, best)
-    b3 = new_par(pop, f, range_rgb, 14, population, best)
+    row3 = new_par(population, F, range_pixel, 10, num_population, best)
+    col3 = new_par(population, F, range_pixel, 11, num_population, best)
+    r3 = new_par(population, F, range_rgb, 12, num_population, best)
+    g3 = new_par(population, F, range_rgb, 13, num_population, best)
+    b3 = new_par(population, F, range_rgb, 14, num_population, best)
 
-    row4 = new_par(pop, f, range_pixel, 15, population, best)
-    col4 = new_par(pop, f, range_pixel, 16, population, best)
-    r4 = new_par(pop, f, range_rgb, 17, population, best)
-    g4 = new_par(pop, f, range_rgb, 18, population, best)
-    b4 = new_par(pop, f, range_rgb, 19, population, best)
+    row4 = new_par(population, F, range_pixel, 15, num_population, best)
+    col4 = new_par(population, F, range_pixel, 16, num_population, best)
+    r4 = new_par(population, F, range_rgb, 17, num_population, best)
+    g4 = new_par(population, F, range_rgb, 18, num_population, best)
+    b4 = new_par(population, F, range_rgb, 19, num_population, best)
 
-    row5 = new_par(pop, f, range_pixel, 20, population, best)
-    col5 = new_par(pop, f, range_pixel, 21, population, best)
-    r5 = new_par(pop, f, range_rgb, 22, population, best)
-    g5 = new_par(pop, f, range_rgb, 23, population, best)
-    b5 = new_par(pop, f, range_rgb, 24, population, best)
+    row5 = new_par(population, F, range_pixel, 20, num_population, best)
+    col5 = new_par(population, F, range_pixel, 21, num_population, best)
+    r5 = new_par(population, F, range_rgb, 22, num_population, best)
+    g5 = new_par(population, F, range_rgb, 23, num_population, best)
+    b5 = new_par(population, F, range_rgb, 24, num_population, best)
 
     return row1, col1, r1, g1, b1, row2, col2, r2, g2, b2, row3, col3, r3, g3, b3, row4, col4, r4, g4, b4, row5, col5, r5, g5, b5
 
-def get_best_individual(old_value, pop, population):
+def get_best_individual(old_value, pop, num_population):
     best_value = old_value[0]
     best = pop[0]
-    population_indeces = range(1, population)
+    population_indeces = range(1, num_population)
     for i in population_indeces:
         if old_value[i][target] < best_value[target]:
             best_value = old_value[i]
@@ -244,55 +243,55 @@ def trasform_to_int(solution):
         best_int.append(int(solution[i]))
     return best_int
 
-def create_population(img, population):
-    pop_array = new_matrix(img, population)
-    pop = []
-    new_pop = []
-    population_indeces = range(0, population)
+def create_population(img, num_population):
+    matrix = new_matrix(img, num_population)
+    population = []
+    new_population = []
+    population_indeces = range(0, num_population)
     for p in population_indeces:
         rand = get_random_input()
-        set_image(pop_array, p, rand)
-        pop.append(rand)
-        new_pop.append(rand)
-    return pop, new_pop, pop_array
+        set_image(matrix, p, rand)
+        population.append(rand)
+        new_population.append(rand)
+    return population, new_population, matrix
 
-def differentialAlgorithm(model, target, img, iterations, population, f, range_pixel, range_rgb, dict):
+def differentialAlgorithm(model, target, img, iterations, num_population, F, range_pixel, range_rgb, dict):
 
-    pop, new_pop, pop_array = create_population(img, population)
+    population, new_population, matrix = create_population(img, num_population)
 
-    old_value = model.predict(pop_array)
+    old_value = model.predict(matrix)
 
-    best = get_best_individual(old_value, pop, population)
+    best = get_best_individual(old_value, population, num_population)
 
-    pop_array = new_matrix(img, population)
+    matrix = new_matrix(img, num_population)
 
     iterations_indeces = range(0, iterations)
     for i in tqdm(iterations_indeces):
-        pop_store = []
-        population_indeces = range(0, population)
+        old_pixel_store = []
+        population_indeces = range(0, num_population)
         for p in population_indeces:
-            new = new_son(pop, f, range_pixel, range_rgb, population, best)
+            new = new_son(population, F, range_pixel, range_rgb, num_population, best)
 
-            #set_image(pop_array, p, new)
-            store = set_image(pop_array, p, new)
-            pop_store.append(store)
+            #set_image(matrix, p, new)
+            store = set_image(matrix, p, new)
+            old_pixel_store.append(store)
 
-            new_pop[p] = new
+            new_population[p] = new
 
-        value = model.predict(pop_array)
+        value = model.predict(matrix)
 
-        #pop_array = new_matrix(img, population)
-        unset_images(pop_store, pop_array, population, new_pop)
+        #matrix = new_matrix(img, num_population)
+        unset_images(old_pixel_store, matrix, num_population, new_population)
 
         for p in population_indeces:
             if value[p][target] < old_value[p][target]:
-                pop[p] = new_pop[p]
+                population[p] = new_population[p]
                 old_value[p] = value[p]
                 if get_max_class_new(old_value[p], dict) != target:
                     print(old_value[p])
-                    return trasform_to_int(pop[p])
+                    return trasform_to_int(population[p])
 
-        best = get_best_individual(old_value, pop, population)
+        best = get_best_individual(old_value, population, num_population)
 
     return trasform_to_int(best)
 
@@ -365,6 +364,7 @@ def fool_image(model, img, img_index, target, number_of_pixel, show_image, dict,
         print("Pixel: (" + str(args[index + 1]) + ", " + str(args[index]) + ")", end=", ")
         print("Rgb: (" + str(args[index + 2]) + ", " + str(args[index + 3]) + ", " + str(args[index + 4]) + ")")
         index += 5
+    string += "\n"
 
     if show_image:
         plt.show()
@@ -405,14 +405,20 @@ model = networks.vgg16.vgg16_cifar10.cifar10vgg()
 #load cifar10 dataset
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-#list = range(start_img_index, end_img_index) #USELESS if you use the random selection:
-
+print_images(x_test, "save/results_non-targeted.txt")
+'''
+list = range(start_img_index, end_img_index) #USELESS if you use the random selection:
+'''
+'''
 #random images
 list = []
 max = len(x_test)
 for i in range(0, num_images):
     list.append(randint(0, max))
-
+'''
+'''
+if save:
+    file = open("save/results_non-targeted.txt", "w")
 
 for img_index in list: #image that will be modified
 
@@ -423,18 +429,16 @@ for img_index in list: #image that will be modified
     #y_train = keras.utils.to_categorical(y_train, 10) #trasform the class into an array (0 .. 1 ... 0)
     #y_test = keras.utils.to_categorical(y_test, 10)
 
-    if save:
-        file = open("save/results_non-targeted.txt", "w")
-
     res = fool_image(model, img, img_index, target, number_of_pixel, show_image, dict, save, file, iterations, population, f, range_pixel, range_rgb)
     print(res)
     if res == True:
         mispredicted_images += 1
 
-    if save:
-        file.close()
+if save:
+    file.close()
 
 #use this function if you want to print all the images in the file Results
 #print_images(x_test, "save/results_non-targeted.txt")
 
 print("Number of mis-predicted images: " + str(mispredicted_images))
+'''
