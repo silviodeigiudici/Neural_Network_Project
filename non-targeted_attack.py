741b03a75ff1ebc47df3745770cd0d58d78fe23a
luckyMan27 <tocci.1695457@studenti.uniroma1.it>
2019-04-12 00:38:36 +0200

    Added some result for non targeted attack.

--- non-targeted_attack.py
+++ non-targeted_attack.py
@@ -304,10 +304,10 @@ def fool_image(model, img, img_index, target, number_of_pixel, show_image, dict,
 dict = { 0:"airplane", 1:"automobile", 2:"bird", 3:"cat", 4:"deer", 5:"dog", 6:"frog", 7:"horse", 8:"ship", 9:"truck"}
 start_img_index = 1 #number of the first image used in cifar10
 end_img_index = 3 #last number (NOT incluted)
-number_of_pixel = 5 #number of pixel that we will try to change
+number_of_pixel = 3 #number of pixel that we will try to change
 show_image = False #IT DOESN'T WORK, USE PRINT IMAGES False = don't show the image
 save = True #if you want to save the result
-num_images = 100 #set the number of images to be extracted
+num_images = 150 #set the number of images to be extracted
 iterations = 100
 population = 400
 crossover = 1
@@ -316,7 +316,7 @@ range_pixel = 32
 range_rgb = 256
 F = 0.5
 dict_nn = {0: "vgg16",1: "NiN",2: "allcnn"}
-neuralnetwork = 0 #0 for vgg16, 1 for nin, 2 for allcnn
+neuralnetwork = 1 #0 for vgg16, 1 for nin, 2 for allcnn
 ###############################
 
 mispredicted_images = 0
